06f01ec20832f9d2681fa574658781e665716078
Brian D. Milotte <brianmilotte@yahoo.com>
2020-09-01 21:44:10 -0400

    google OAuth 2.0, deprecated but works today

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
+const findOrCreate = require("mongoose-findorcreate");
 //lvl 4
 //went to bcrypt hashing instead of mongoose-encryption
 // const encrypt = require("mongoose-encryption");
@@ -15,9 +16,9 @@ const mongoose = require("mongoose");
 // const bcrypt = require("bcrypt");
 // const saltRounds = 10;
 
-// lvl 6.  passport / sessions
+// lvl 6.  passport / sessions /google-OAuth2.0
 //passport docs http://www.passportjs.org/
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
@@ -30,10 +31,11 @@ app.use(bodyParser.urlencoded({
 
 //initialize session here
 app.use(session({
-    secret: "secretssssssss",
+    secret: process.env.secret,
     resave: false,
     saveUninitialized: false
 }));
+
 //initialize passport here
 app.use(passport.initialize());
 app.use(passport.session());
@@ -43,20 +45,51 @@ mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, us
 mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 //removed mongoose.encryption module and went to HASH
 // userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
 
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        done(err, user);
+    });
+});
+//GOOGLE STRATEGY IS DEPRECATED, DONT USE IT!!
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+},
+function(accessToken, refreshToken, profile, cb){
+    User.findOrCreate({googleId: profile.id}, function(err, user){
+        return cb(err, user);
+    });
+}
+));
 app.get("/", function (req, res) {
     res.render("home");
+});
+app.get("/auth/google", 
+passport.authenticate("google", {scope: ['profile']})
+);
+
+app.get("/auth/google/secrets", 
+passport.authenticate("google", {failureRedirect: "/login"}),
+function(req, res){
+    res.redirect("/secrets");
 });
 app.get("/login", function (req, res) {
     res.render("login");
@@ -72,8 +105,13 @@ else { res.redirect("/login");
 }
 });
 app.get("/logout", function(req, res){
-    req.logout();
-    res.redirect("/");
+    req.session.destroy((err) => {
+        if (!err){
+            req.logout();    
+            // res.sendStatus(200);
+            res.redirect("/");
+        }
+    });
 });
 
 
